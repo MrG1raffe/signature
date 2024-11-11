dd6d07740f908a137285cebbd2b8694bce884069
Dimitri Sotnikov <dmitrysotni@gmail.com>
2024-11-11 17:48:24 +0100

    rename folder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='signatures',
+   name='signature',
    version='1.0',
    description='Option pricing models',
    author='MrG1raffe',
